f34017404e72e6c34f4f538b8aa75f03f454ed19
Victor-LEONARD <leonard@abvent.fr>
2018-11-16 11:42:24 +0100

    trackList: display only top selection

--- client/src/pages/TrackList.vue
+++ client/src/pages/TrackList.vue
@@ -2,7 +2,7 @@
   <q-page padding>
     <q-list highlight>
     <q-list-header>Playlist</q-list-header>
-    <q-collapsible v-for="s in selections" :key="s._id" :avatar="s.track.album.images[2].url" :label="s.track.artists[0].name + ' - ' + s.track.name">
+    <q-collapsible v-for="s in allVoteOk" :key="s._id" :avatar="s.track.album.images[2].url" :label="s.track.artists[0].name + ' - ' + s.track.name">
       <div class="row items-center">
         <div class="col col-auto" style="margin: 0 auto">
           <q-btn push rounded color="primary" label="Play" icon="ion-md-play" />
@@ -10,28 +10,16 @@
         </div>
         <div class="col col-auto" style="margin: 0 auto">
           <small>Album: <span>{{ s.track.album.name }}</span></small><br>
-          <small>Ajouté le 04/09/2018 par Julien</small><br>
-          <q-rating slot="subtitle" v-model="rating.muse" readonly :max="5" />
+          <small>Ajouté le {{ formatDate(s.creationDate) }} par {{ getUserName(s.userId) }}</small><br>
+          <q-rating slot="subtitle" :value="getAverage(s.vote)" readonly :max="5" />
           <br><br>
         </div>
       </div>
 
-      <div class="row">
-        <div class="col q-pa-sm">
-        <div>Victor</div>
-          <q-rating slot="subtitle" v-model="rating.muse" readonly :max="5" />
-        </div>
-        <div class="col q-pa-sm">
-        <div>Victor</div>
-          <q-rating slot="subtitle" v-model="rating.muse" readonly :max="5" />
-        </div>
-        <div class="col q-pa-sm">
-        <div>Victor</div>
-          <q-rating slot="subtitle" v-model="rating.muse" readonly :max="5" />
-        </div>
-        <div class="col q-pa-sm">
-        <div>Victor</div>
-          <q-rating slot="subtitle" v-model="rating.muse" readonly :max="5" />
+      <div class="row" v-if="s.vote.length">
+        <div class="col q-pa-sm" v-for="vote in s.vote" :key="vote.userId">
+        <div>{{ getUserName(vote.userId) }}</div>
+          <q-rating slot="subtitle" :value="vote.value" readonly :max="5" />
         </div>
       </div>
     </q-collapsible>
@@ -43,6 +31,7 @@
 
 import Api from '../services/Api'
 import { Loading } from 'quasar'
+import moment from 'moment'
 
 export default {
   data () {
@@ -66,20 +55,20 @@ export default {
       if (this.selections) {
         return this.selections.filter(el => {
           let len = el.vote.length
-          return len === 4
+          return len === this.users.length
         })
       }
     },
     allVoteOk () {
+      let average = (this.users.length * 5) / 2
       if (this.allVote) {
         let els = []
         this.allVote.forEach(el => {
-          console.log(el)
           let total = 0
           el.vote.forEach(v => {
-            total += v.vote
+            total += v.value
           })
-          if (total >= 10) {
+          if (total >= average) {
             els.push(el)
           }
         })
@@ -88,6 +77,28 @@ export default {
     }
   },
   methods: {
+    getAverage (votes) {
+      let total = 0
+      votes.forEach(v => {
+        total += v.value
+      })
+      return total / votes.length
+    },
+    formatDate (date) {
+      return moment(date).lang('fr').format('L')
+    },
+    getUserName (userId) {
+      let userName = this.users.find(user => user._id === userId)
+      if (userName) {
+        return userName.name
+      }
+    },
+    getUsers () {
+      Api().get('users')
+        .then(resp => {
+          this.users = resp.data.users
+        })
+    },
     getSelections () {
       Loading.show()
       Api().get('selection')
@@ -102,6 +113,7 @@ export default {
     }
   },
   mounted () {
+    this.getUsers()
     this.getSelections()
   }
 }
