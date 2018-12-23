8b5e4c6b6d3877af0aeb4c20152e50c1dd49b3cb
Victor-LEONARD <leonard@abvent.fr>
2018-12-23 23:59:58 +0100

    style

--- client/src/App.vue
+++ client/src/App.vue
@@ -39,7 +39,7 @@ export default {
     }
   },
   mounted () {
-    this.checkVersion()
+    // this.checkVersion()
     // get local storage app version
     // get serveur app version
     // if !== ==> window.location.reload(true)
@@ -48,4 +48,72 @@ export default {
 </script>
 
 <style>
+@media only screen and (min-device-width : 375px) and (max-device-width : 812px) and (-webkit-device-pixel-ratio : 3) {
+    .q-tabs-head {
+      padding-bottom: 10px !important;
+    }
+  }
+  .q-rating {
+    color: #f57f17 !important;
+  }
+  .q-rating i {
+    text-shadow: none;
+  }
+  .q-tabs-inverted .q-tabs-head{
+    background-color: rgba(255, 255, 255, 0.95)
+  }
+  .q-tabs-scroller {
+    color: #444
+  }
+  hr {
+    height: 1px;
+    padding: 0;
+    background-color: #aaa;
+    border: 0;
+  }
+  .search-page {
+    background-color: #F5F5F5;
+  }
+  .search-page .q-item-letter {
+    position: absolute;
+    top: 0;
+    right: 0;
+    border-radius: 0px 3px 0px 0px;
+    background: #616161 !important;
+    padding: 3px 5px;
+    font-size: 12px;
+    color: #fff;
+  }
+  .search-page .q-collapsible-sub-item {
+    background-color: #FAFAFA;
+    border-top: 1px solid rgba(0,0,0,0.1)
+  }
+  .search-page .q-list {
+    padding: 0;
+    border: none;
+  }
+  .search-page .q-item-side {
+    overflow: hidden;
+  }
+  .search-page .q-item-main {
+    margin-left: -20px;
+  }
+  .search-page .q-item-side img {
+    height: 80px;
+    widows: 80px;
+    object-fit: contain;
+    object-position: left;
+  }
+  .search-page .q-collapsible {
+    margin-bottom: 10px;
+  }
+  .search-page .q-collapsible-inner {
+    background-color: #fff;
+  }
+  .search-page .q-collapsible-cursor-pointer > .q-collapsible-inner > .q-item {
+    padding: 0 !important;
+  }
+  .search-page .q-collapsible-cursor-pointer > .q-collapsible-inner > .q-item img {
+    display: block;
+  }
 </style>
