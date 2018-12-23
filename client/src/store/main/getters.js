25f6ed074996ba92d16f896c6afc130c5b2da11a
Victor-LEONARD <leonard@abvent.fr>
2018-12-23 23:59:10 +0100

    getters

--- client/src/store/main/getters.js
+++ client/src/store/main/getters.js
@@ -1,3 +1,5 @@
+import orderBy from 'lodash/orderBy'
+
 export function average (state) {
   if (state.users) {
     return parseInt((state.users.length * 5) / 1.2)
@@ -16,10 +18,30 @@ export function vetoVote (state) {
   }
 }
 
-export function awaitingVote (state) {
+export function selectionWithoutVeto (state) {
+  if (state.songsList) {
+    let result = []
+    state.songsList.forEach(selection => {
+      if (!selection.vote.find(vote => vote.value === 0)) {
+        result.push(selection)
+      }
+    })
+    return orderBy(result, ['creationDate'], ['desc'])
+  }
+}
+
+export function awaitingVote (state, getters) {
+  if (state.songsList && state.users && getters.selectionWithoutVeto) {
+    return getters.selectionWithoutVeto.filter(el => {
+      return el.vote.length < state.users.length
+    })
+  }
+}
+
+export function allVote (state) {
   if (state.songsList && state.users) {
     return state.songsList.filter(el => {
-      return el.vote.length < state.users.length
+      return el.vote.length === state.users.length
     })
   }
 }
