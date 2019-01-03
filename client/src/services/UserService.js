8b6f695d8c0e3c1968192935387138c8597652d7
Victor-LEONARD <leonard@abvent.fr>
2019-01-03 20:47:14 +0100

    userService

--- client/src/services/UserService.js
+++ client/src/services/UserService.js
@@ -4,9 +4,18 @@ export default {
   getUsers () {
     return Api().get('/users')
   },
+  getUser (userId) {
+    return Api().get('/users/' + userId)
+  },
   createUser (username, email, password) {
     return Api().post('/users', {
       'username': username,
+      'email': email,
+      'password': password
+    })
+  },
+  logIn (email, password) {
+    return Api().post('/users/login', {
       'email': email,
       'password': password
     })
