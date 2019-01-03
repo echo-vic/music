bb53f985c55dfa1428b0322bdb8aee51f8305b1e
Victor-LEONARD <leonard@abvent.fr>
2019-01-03 20:49:40 +0100

    api config: add acces token acces

--- client/src/services/Api.js
+++ client/src/services/Api.js
@@ -1,7 +1,12 @@
 import axios from 'axios'
 
 export default() => {
+  console.log('token', localStorage.token)
   return axios.create({
-    baseURL: `http://localhost:3001`
+    baseURL: `http://localhost:3000/api`,
+    // baseURL: `https://boiling-refuge-28644.herokuapp.com`
+    params: {
+      'access_token': localStorage.token
+    }
   })
 }
