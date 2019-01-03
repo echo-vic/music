73d4e34b71ce05f01fb5950f57149ff2ae232e28
Victor-LEONARD <leonard@abvent.fr>
2019-01-03 20:47:56 +0100

    new home

--- client/src/pages/Home.vue
+++ client/src/pages/Home.vue
@@ -1,72 +1,18 @@
 <template>
   <q-page padding>
-    <q-modal v-model="modal" minimized v-if="tempUser">
-      <div style="padding: 50px">
-        <div class="q-display-1 q-mb-md">Bienvenue !</div>
-        <p>Mais es-tu vraiment <span style="font-weight: bold">{{ tempUser.name }}</span> ?</p>
-        <q-btn @click="connect" style="margin:15px 15px 15px 0px" color="blue" label="Oui !" />
-        <q-btn color="red" v-close-overlay label="Heu en fait non..." />
-      </div>
-  </q-modal>
       <!--<h3 class="text-grey-8">Qui es-tu ?</h3>-->
       <div class="row justify-center">
-        <div class="q-pa-sm col col-6" v-for="user in users" :key="user.name" @click="identify(user)">
-          <q-card v-ripple class="relative-position">
-            <q-card-media overlay-position="top" :height="150">
-              <img :src="user.avatar">
-            </q-card-media>
-            <q-card-title class="relative-position">
-              <q-btn class="bg-indigo-8" round size="md" color="primary" style="position: absolute; top: 0px; right: 8px; transform: translateY(-50%);">
-                <div @click="identify(user)" class="q-btn-inner row col items-center justify-center">
-                <i aria-hidden="true" class="q-icon material-icons">check</i>
-                </div>
-              </q-btn>
-              <div>
-                {{ user.name }}
-              </div>
-            </q-card-title>
-          </q-card>
-        </div>
+        <div v-if="user">Bienvenue {{user.username}}</div>
       </div>
   </q-page>
 </template>
 
 <script>
-import Api from '../services/Api'
+import { mapState } from 'vuex'
 
 export default {
-  data () {
-    return {
-      tempUser: undefined,
-      modal: false,
-      users: [],
-      currentUser: localStorage.getItem('userId'),
-      currentUserName: localStorage.getItem('userName')
-    }
-  },
-  methods: {
-    identify (user) {
-      this.tempUser = user
-      this.modal = true
-    },
-    connect () {
-      this.currentUser = this.tempUser._id
-      this.currentUserName = this.tempUser.name
-      localStorage.setItem('userId', this.tempUser._id)
-      localStorage.setItem('userName', this.tempUser.name)
-      this.modal = false
-      this.$router.push({
-        name: 'list'
-      })
-      location.reload()
-    }
-  },
-  mounted () {
-    Api().get('users')
-      .then(resp => {
-        console.log('user', resp)
-        this.users = resp.data.users
-      })
+  computed: {
+    ...mapState('main', ['user'])
   }
 }
 </script>
