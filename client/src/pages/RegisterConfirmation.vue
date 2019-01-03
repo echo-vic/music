ac8418d010e172515a48c69e07fb004b7c23cd63
Victor-LEONARD <leonard@abvent.fr>
2019-01-03 20:48:42 +0100

    component() register confirmation

---
+++ client/src/pages/RegisterConfirmation.vue
@@ -0,0 +1,21 @@
+<template>
+  <q-page padding>
+      <div class="row justify-center">
+        <q-jumbotron>
+          <div class="q-display-3">Bienvenue !</div>
+          <div class="q-subheading">
+            Votre adresse email a bien été verifiée
+          </div>
+          <hr class="q-hr q-my-lg">
+          <router-link to="/connect">
+            <q-btn color="primary" class="q-py-sm q-px-xl full-width" label="Se connecter" />
+          </router-link>
+        </q-jumbotron>
+      </div>
+  </q-page>
+</template>
+
+<script>
+
+export default {}
+</script>
