801aa83433360591e9280d01a1fbad0c67ed16e4
Victor-LEONARD <leonard@abvent.fr>
2019-01-03 20:47:04 +0100

    vuex() add users action

--- client/src/store/main/actions.js
+++ client/src/store/main/actions.js
@@ -1,6 +1,10 @@
 import SongService from './../../services/SongService'
 import UserService from './../../services/UserService'
 
+export function changeLoadingState ({commit}, state) {
+  commit('CHANGE_LOADING_STATE', state)
+}
+
 export async function loadSongs ({commit}, silent) {
   if (!silent) {
     commit('CHANGE_LOADING_STATE', true)
@@ -35,4 +39,34 @@ export async function createUser ({commit, dispatch}, {username, email, password
   } else {
     console.error('error')
   }
+}
+
+export async function connectUser ({commit, dispatch}, {email, password}) {
+  commit('CHANGE_LOADING_STATE', true)
+  const response = await UserService.logIn(email, password)
+  if (response) {
+    console.log('connectUSer', response)
+    localStorage.setItem('token', response.data.id)
+    commit('UPDATE_TOKEN', response.data.id)
+    dispatch('getUser', {
+      userId: response.data.userId
+    })
+    // commit('UPDATE_USER', response.data)
+    commit('CHANGE_LOADING_STATE', false)
+    return response
+  } else {
+    console.error('error')
+  }
+}
+
+export async function getUser ({commit}, {userId}) {
+  UserService.getUser(userId)
+    .then(resp => {
+      commit('UPDATE_USER', resp.data)
+      localStorage.setItem('userId', resp.data.id)
+      localStorage.setItem('userEmail', resp.data.email)
+    })
+    .catch(error => {
+      console.error('error', error)
+    })
 }
