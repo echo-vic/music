f815988ce1682e67d7c4ca97be01e58a5ad21c0e
Victor-LEONARD <leonard@abvent.fr>
2019-01-05 23:26:17 +0100

    vuex actions

--- client/src/store/main/actions.js
+++ client/src/store/main/actions.js
@@ -1,5 +1,6 @@
 import SongService from './../../services/SongService'
 import UserService from './../../services/UserService'
+import GroupService from './../../services/GroupService'
 
 export function changeLoadingState ({commit}, state) {
   commit('CHANGE_LOADING_STATE', state)
@@ -24,6 +25,37 @@ export async function loadUsers ({commit}) {
   UserService.getUsers()
     .then(resp => {
       commit('UPDATE_USERS', resp.data.users)
+    })
+    .catch(error => {
+      console.error('error', error)
+    })
+}
+
+/* export async function loadMyGroups ({commit, state}) {
+  GroupService.getMyGroups(state.user.id)
+    .then(resp => {
+      commit('UPDATE_MY_GROUPS', resp.data)
+    })
+    .catch(error => {
+      console.error('error', error)
+    })
+} */
+
+/* export async function loadMembersGroup ({commit, state}, {groupId}) {
+  GroupService.getMembers(groupId)
+    .then(resp => {
+      commit('UPDATE_MY_GROUP', resp.data)
+    })
+    .catch(error => {
+      console.error('error', error)
+    })
+} */
+
+export async function loadMyCreatorGroups ({commit, state}) {
+  console.log('state.user.id', state.user.id)
+  GroupService.getMyCreatorGroups(state.user.id)
+    .then(resp => {
+      commit('UPDATE_MY_CREATOR_GROUPS', resp.data)
     })
     .catch(error => {
       console.error('error', error)
@@ -59,14 +91,41 @@ export async function connectUser ({commit, dispatch}, {email, password}) {
   }
 }
 
-export async function getUser ({commit}, {userId}) {
+export async function getUser ({commit, dispatch}, {userId}) {
   UserService.getUser(userId)
     .then(resp => {
       commit('UPDATE_USER', resp.data)
       localStorage.setItem('userId', resp.data.id)
       localStorage.setItem('userEmail', resp.data.email)
+      dispatch('loadMyCreatorGroups')
+      dispatch('loadMyGroups')
     })
     .catch(error => {
       console.error('error', error)
     })
+}
+
+export async function createGroup ({commit, dispatch}, {name, genre, departement, commune, level, creatorId, instrument, username}) {
+  commit('CHANGE_LOADING_STATE', true)
+  console.log('createGroue action', instrument)
+  const response = await GroupService.createGroup(name, genre, departement, commune, level, creatorId, instrument, username)
+  if (response) {
+    dispatch('loadMyCreatorGroups')
+    commit('CHANGE_LOADING_STATE', false)
+    return response.data
+  } else {
+    console.error('error')
+  }
+}
+
+export async function deleteMyCreatorGroup ({commit, dispatch}, {groupId}) {
+  commit('CHANGE_LOADING_STATE', true)
+  const response = await GroupService.deleteMyGroup(groupId)
+  if (response) {
+    dispatch('loadMyCreatorGroups')
+    commit('CHANGE_LOADING_STATE', false)
+    return response.data
+  } else {
+    console.error('error')
+  }
 }
