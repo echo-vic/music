4a3cb3b6d77ccdeee72a57a3654d74a00b20cedb
Victor-LEONARD <leonard@abvent.fr>
2019-01-05 23:27:09 +0100

    router: add group creation

--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -5,14 +5,15 @@ const routes = [
     path: '/',
     component: () => import('layouts/MyLayout.vue'),
     children: [
-      { path: !userid ? '' : 'home', name: 'home', component: () => import('pages/Home.vue') },
+      { path: 'home', name: 'home', component: () => import('pages/Home.vue') },
       { path: 'register-confirmation', name: 'RegisterConfirmation', component: () => import('pages/RegisterConfirmation.vue') },
       { path: !userid ? 'list' : '', name: 'list', component: () => import('pages/TrackList.vue') },
       { path: 'vote', name: 'vote', component: () => import('pages/Vote.vue') },
       { path: 'search', name: 'search', component: () => import('pages/Search.vue') },
       { path: 'register', name: 'register', component: () => import('pages/Register.vue') },
       { path: 'connect', name: 'connect', component: () => import('pages/Connect.vue') },
-      { path: 'repet', name: 'repet', component: () => import('pages/Repet.vue') }
+      { path: 'repet', name: 'repet', component: () => import('pages/Repet.vue') },
+      { path: 'group-creation', name: 'groupCreation', component: () => import('pages/GroupCreation.vue') }
     ]
   }
 ]
